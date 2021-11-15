fbecd5d0d3d48ed71c367f258c82695081d36990
shenxin <shenxin@sandboxol.com>
2021-11-15 19:35:49 +0800

    修改 cmake. 生成带文件目录结构的项目文件

--- LearnDirectX12/cmake_all.bat
+++ LearnDirectX12/cmake_all.bat
@@ -2,10 +2,12 @@
 rd /s /q %~dp0Build
 mkdir Build
 set /P OPT=<cmake_option.txt
-for /d %%i in (*) do (
-	if exist %%i/CMakeLists.txt (
-		mkdir %~dp0Build\%%i
-		cmake -S %~dp0%%i %OPT% -B Build/%%i
-	)
-)
+rem for /d %%i in (*) do (
+rem 	if exist %%i/CMakeLists.txt (
+rem 		mkdir %~dp0Build\%%i
+rem 		cmake -S %~dp0%%i %OPT% -B Build/%%i
+rem 	)
+rem )
+
+cmake -S %~dp0 %OPT% -B Build
 pause
